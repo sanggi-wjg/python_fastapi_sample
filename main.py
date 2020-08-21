4322e0c745bb5b9a7ed76e2814319e4e210931ea
jay_g <jay_g@kr.accommate.com>
2020-08-21 09:33:06 +0900

    2020 08 21

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ async def get_index():
     return { 'Hello': 'World' }
 
 
-@app.get('/item/{item_id}')
+@app.get('/item/{item_id}', name = 'item_id 정보 가져오기', description = 'get_item description')
 async def get_item(item_id: int, q: Optional[str] = None):
     return { 'item_id': item_id, 'q': q }
 
 
-@app.put('/item/{item_id}')
+@app.put('/item/{item_id}', name = 'item_id 정보 수정')
 async def put_item(item_id: int, item: Item):
     return { 'item_id': item_id, 'item_name': item.name }
